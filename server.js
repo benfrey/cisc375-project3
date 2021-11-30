75fa9eab30405aa4b8bb2014e296f1abbf951e16
Grant <toen1806@stthomas.edu>
2021-11-29 18:14:05 -0600

    incidents get function almost complete

--- server.js
+++ server.js
@@ -106,9 +106,64 @@ app.get('/incidents', (req, res) => {
   sqlQuery = 'SELECT * FROM Incidents';
 
   // Specific query (logan and grant)
+  //this case_number is not specifically needed, just using for testing purposes
+  if(req.query.case_number){
+    sqlQuery+= ' WHERE case_number= "'+req.query.case_number+'"';
+  }
 
+  if(req.query.neighborhood){
+    nieghborhoodArray = req.query.neighborhood.split(",");
+    sqlQuery += " WHERE neighborhood_number = " + nieghborhoodArray.join(" OR neighborhood_number = ");
+  }
+
+  if(req.query.grid){
+    gridArray = req.query.grid.split(",");
+    if(req.query.neighborhood){
+      sqlQuery += " AND police_grid = " + gridArray.join(" OR police_grid = ");
+    }else{
+      sqlQuery += " WHERE police_grid = " + gridArray.join(" OR police_grid = ");
+    }
+  }
+
+  if(req.query.code){
+    codeArray = req.query.code.split(",");
+    if(req.query.grid || req.query.neighborhood){
+      sqlQuery += " AND code = " + codeArray.join(" OR code = ");
+    }else{
+      sqlQuery += " WHERE code = " + codeArray.join(" OR code = ");
+    }
+  }
+
+  if(req.query.start_date && req.query.end_date){
+    if(req.query.code || req.query.grid || req.query.neighborhood){
+      sqlQuery += " AND date_time BETWEEN "+req.query.start_date+" and "+req.query.end_date;
+    }else{
+      sqlQuery += " WHERE date_time BETWEEN "+req.query.start_date+" and "+req.query.end_date;
+    }
+  }else if(req.query.end_date){
+    if(req.query.code || req.query.grid || req.query.neighborhood){
+      sqlQuery += " AND date_time <= "+req.query.end_date;
+    }else{
+      sqlQuery += " WHERE date_time <= "+req.query.end_date;
+    }
+  }else if(req.query.start_date){
+    if(req.query.code || req.query.grid || req.query.neighborhood){
+      sqlQuery += " AND date_time >= "+req.query.start_date;
+    }else{
+      sqlQuery += " WHERE date_time >= "+req.query.start_date;
+    }
+  }
+    
   // End of sql query statement
   sqlQuery += " ORDER BY date_time";
+
+  if(req.query.limit){
+    sqlQuery += ' LIMIT '+req.query.limit;
+  } else {
+    sqlQuery += ' LIMIT 1000';
+  }
+
+  console.log(sqlQuery);
 
   // Make the database query
   new Promise( (resolve, reject) => {
@@ -126,21 +181,17 @@ app.get('/incidents', (req, res) => {
   }).catch(err => {
     res.status(500).send("Error querying database");
   });
-
-  db.all('SELECT * FROM Incidents ORDER BY date_time', (err, rows) => {
-    res.status(200).type('json').send(rows); // Still need newline after each row
-  });
 });
 
 // Put /new-incident
 
 app.put('/new-incident', (req, res) =>{
     console.log(req.body);
-    db.get('SELECT * FROM Incidents WHERE case_number = ?', [req.body.caseNumber], (err, row)=> {
+    db.get('SELECT * FROM Incidents WHERE case_number = ?', [req.body.case_number], (err, row)=> {
         if(err || row !== undefined ) {
             res.status(500).type('txt').send('Error, could not insert incident');
         } else {
-            db.run('INSERT INTO Incidents (case_number, date_time, code, indicent, police_grid, neighborhood_number, block) VALUES(?, ?, ?, ?, ?, ?, ?)',[req.body.caseNumber, req.body.dateTime, req.body.code, req.body.incident, req.body.policeGrid, req.body.neighborhoodNumber, req.body.block], (err) =>{
+            db.run('INSERT INTO Incidents (case_number, date_time, code, indicent, police_grid, neighborhood_number, block) VALUES(?, ?, ?, ?, ?, ?, ?)',[req.body.case_number, req.body.date_time, req.body.code, req.body.incident, req.body.police_grid, req.body.neighborhoood_number, req.body.block], (err) =>{
                 res.status(200).type('txt').send('success');
             });
         }
